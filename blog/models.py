3913fa2af88df28a394d3f7495b01633f15b8e02
whyNat <nwaszak@wp.pl>
2019-09-08 17:33:00 +0200

    improvements

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,9 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
     def __str__(self):
         return self.title
 
@@ -34,5 +37,4 @@ class Comment(models.Model):
         return self.text
 
 
-def approved_comments(self):
-    return self.comments.filter(approved_comment=True)
+
